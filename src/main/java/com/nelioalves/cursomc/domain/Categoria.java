1752b2246ae1b535d5ea219e22b433096f601f48
Andrews S. Fernandes <andrews.fernandesx@gmail.com>
2021-01-04 11:43:56 -0300

    Implementado CommandLineRunner para criar instância e criarregistros automáticos na tabela categoria no banco h2 para fins de testes.

--- src/main/java/com/nelioalves/cursomc/domain/Categoria.java
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 
@@ -16,6 +18,7 @@ public class Categoria implements Serializable {
   public static final long serivalVersionUID = 1L;
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
   private String nome;
 
